869f6dd7a00306facf005f6d52112492d2848008
Rabin Shrestha <sanayashe@gmail.com>
2023-10-27 16:03:44 +0545

    form post

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,26 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+	const [newItemName, setNewItemName] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <h4>task bud</h4>
-      <div className='form-control'>
-        <input
-          type='text '
-          className='form-input'
-          value={newItemName}
-          onChange={(event) => setNewItemName(event.target.value)}
-        />
-        <button type='submit' className='btn'>
-          add task
-        </button>
-      </div>
-    </form>
-  );
+	const { mutate: createTask, isLoading } = useMutation({
+		mutationFn: () => customFetch.post("/", { title: "some title" }),
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<h4>task bud</h4>
+			<div className="form-control">
+				<input
+					type="text "
+					className="form-input"
+					value={newItemName}
+					onChange={(event) => setNewItemName(event.target.value)}
+				/>
+				<button type="submit" className="btn" disabled={isLoading}>
+					add task
+				</button>
+			</div>
+		</form>
+	);
 };
 export default Form;
