885185d8d50bc68aae010cbee5d78341fc126187
Rabin Shrestha <sanayashe@gmail.com>
2023-10-27 13:34:13 +0545

    queryclient instance

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const QueryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<QueryClientProvider client={QueryClient}>
+		<App />
+	</QueryClientProvider>
+);
